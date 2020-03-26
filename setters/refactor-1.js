593effd53b72fe24b9466b32f832ddc6a154709b
thomashaile <gthomashaile@gmail.com>
2020-03-27 00:31:19 +0100

    setters

--- setters/refactor-1.js
+++ setters/refactor-1.js
@@ -1,25 +1,26 @@
 const obj1 = {
-  greeting: ``,
-  setGreetingName: function (newName) {
-    this.greeting = `hi, I'm ${newName}!`;
-  }
+    greeting: ``,
+    setGreetingName: function(newName) {
+        this.greeting = `hi, I'm ${newName}!`;
+    }
 };
 
 const obj2 = {
-  greeting: ``,
-  set greetingName(newName) {
-    // write me!
-  }
+    greeting: ``,
+    set greetingName(newName) {
+        // write me!
+        this.greeting = `hi, I'm ${newName}!`;
+    }
 };
 
 obj1.setGreetingName('obj1');
 console.assert(obj1.greeting === "hi, I'm obj1!", 'Test 1');
 
-; // write this line
+obj2.greetingName = 'obj2'; // write this line
 console.assert(obj2.greeting === "hi, I'm obj2!", 'Test 2');
 
 obj1.setGreetingName('hi');
 console.assert(obj1.greeting === "hi, I'm hi!", 'Test 3');
 
-; // write this line
+obj2.greetingName = 'bye'; // write this line
 console.assert(obj2.greeting === "hi, I'm bye!", 'Test 4');
